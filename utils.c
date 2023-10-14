1eb4f60094851d8c4ea57aa454b5c17cc0459680
Lea Yeh <lea.yeh.ml@gmail.com>
2023-10-14 16:17:21 +0200

    chore: Remove useless function

--- utils.c
+++ utils.c
@@ -6,29 +6,6 @@
 #include <unistd.h>
 #include "hotrace.h"
 
-u_int32_t	get_dict_size(int fd)
-{
-	char		buffer[BUFFER_SIZE];
-	u_int32_t	line_cnt;
-    int			bytes_read;
-	int			i;
-	bool		meet_newline;
-
-	line_cnt = 0;
-	meet_newline = false;
-	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	while (bytes_read > 0)
-	{
-		while (i++ < bytes_read)
-		{
-			if (buffer[i] == '\n')
-                line_cnt++;
-		}
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
-    }
-    close(fd);
-	return (line_cnt / 2);
-}
 int	ft_strncmp(const char *s1, const char *s2, size_t len)
 {
 	size_t	i;
