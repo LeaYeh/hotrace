cabae1d03215e7745e8c6563b5de565b0d3c02aa
iengels <immanuel.engels@scheef.tech>
2023-10-15 12:38:00 +0200

    changed gnl

--- string_utils.c
+++ string_utils.c
@@ -47,20 +47,14 @@ char	*ft_strdup(const char *s1)
 	return (dst);
 }
 
-int	ft_strncmp(const char *s1, const char *s2, size_t len)
+int	ft_strcmp(const char *s1, const char *s2)
 {
-	size_t	i;
-
-	i = 0;
-	while (i < len && s1[i] && s2[i])
+	while (*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
 	{
-		if ((unsigned char)s1[i] != (unsigned char)s2[i])
-			break ;
-		i++;
+		s1++;
+		s2++;
 	}
-	if (i == len)
-		return (0);
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	return (*s1 - *s2);
 }
 
 size_t	ft_strlen(const char *str)
